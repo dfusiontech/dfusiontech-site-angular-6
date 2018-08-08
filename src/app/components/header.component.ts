dee795ad63982de473a82a165b65cca491289dcd
llysenko-pc\user <ragdor1996@gmail.com>
2018-08-08 19:47:18 +0300

    + header

--- src/app/components/header.component.ts
+++ src/app/components/header.component.ts
@@ -1,15 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-    selector: '[id="header"]',
+    // selector: '[id="header"]',
     templateUrl: './header.html'
 })
-export class HeaderComponentt implements OnInit {
+export class HeaderComponent implements OnInit {
     // initially menu icon isn't triggered
     public menuTrigger = false;
-    constructor() { }
 
+    constructor() {}
     ngOnInit() {
     }
-
 }
