3d45029d8bf74227ffe9b7be718c34a253da987e
llysenko-pc\user <ragdor1996@gmail.com>
2018-08-13 17:07:42 +0300

    + cases and thought constants

---
+++ src/app/constants/cases.constant.ts
@@ -0,0 +1,42 @@
+/**
+ * Cases section data
+ */
+export const CasesConstant = [
+    {
+        title: 'Unreel.me',
+        description: 'Scalable and extremly secured bitcoin front office solution for cash exchange. Allows users' +
+        ' to easily trade bitcoins for a number of foreign currencies. Modular architecture makes …',
+        img: ''
+    },
+    {
+        title: 'Bottles Waiting',
+        description: 'The best way to learn multiplication table with fun and engaging way for children. ' +
+        'Intended for the improvement and testing of children calculation skills.',
+        img: ''
+    },
+    {
+        title: 'Codefresh',
+        description: 'STS is a new full tournament management system that helps improve the professional level' +
+        ' of sports tournament promotions to compete with the top sports offered to children and adults alike.',
+        img: ''
+    },
+    {
+        title: 'Lincoln Center',
+        description: 'This is a platform to collect, store, aggregate, share videos, create your own TV channel' +
+        ' out of YouTube videos, broadcast live from your mobile phone',
+        img: ''
+    },
+    {
+        title: 'Almentor',
+        description: 'STS is a new full tournament management system that helps improve the professional level' +
+        ' of sports tournament promotions to compete with the top sports offered to..',
+        img: ''
+    },
+    {
+        title: 'worldtv',
+        description: 'The best way to learn multiplication table with fun and engaging way for children. ' +
+        'Intended for the improvement and testing of children calculation skills and engaging way for children.',
+        date: 'may 4, 2018',
+        img: ''
+    }
+];
