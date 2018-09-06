c3254299ac148eaae585f060308041c876e530e6
Daniel <workkd97@gmail.com>
2018-09-06 16:59:53 +0300

    [+] implemented routing to different [thoughts]; [+] added a new method [ThoughtsService] to take one concrete [thought] from all; [+] remaked [thought-item] state;

--- src/app/states/thoughts-item/thoughts-item.state.ts
+++ src/app/states/thoughts-item/thoughts-item.state.ts
@@ -1,12 +1,22 @@
-import { ThoughtsItemPage } from "./thoughts-item.page";
-import { HeaderComponent } from "../../components/header/header.component";
-
+import { ThoughtsItemPage } from './thoughts-item.page';
+import { HeaderComponent } from '../../components/header/header.component';
+import { Transition } from '@uirouter/angular';
 
 export const thoughtsItemState = {
-    name: 'thoughtsItem',
-    url: '/thoughtsItem', // there should be single case name or number
+    name: 'thoughts-item',
+    url: '/thought/:thoughtId', // there should be single case name or number
     views: {
         header: { component: HeaderComponent },
         $default: { component: ThoughtsItemPage },
     },
+    resolve: [
+        {
+            token: 'thoughtId',
+            deps: [Transition],
+            resolveFn: (trans) => {
+                // ThoughtsService.getThoughtByLink(trans.params().thoughtId);
+                return trans.params().thoughtId;
+            }
+        }
+    ]
 };
